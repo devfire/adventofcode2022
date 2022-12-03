971a2cfe48c19ce31d5970a4a05cd4ffe7bd00b8
Igor <igor@devfire.io>
2022-12-03 10:26:32 -0500

    removed target

--- day-2/target/debug/day-2.d
+++
@@ -1,1 +0,0 @@
-/home/ig/Documents/rust/adventofcode2022/day-2/target/debug/day-2: /home/ig/Documents/rust/adventofcode2022/day-2/src/main.rs
--- day-2/target/debug/deps/day_2-1d56fa4a9d43d589.d
+++
@@ -1,5 +0,0 @@
-/home/ig/Documents/rust/adventofcode2022/day-2/target/debug/deps/day_2-1d56fa4a9d43d589.rmeta: src/main.rs
-
-/home/ig/Documents/rust/adventofcode2022/day-2/target/debug/deps/day_2-1d56fa4a9d43d589.d: src/main.rs
-
-src/main.rs:
