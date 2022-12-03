b808f70f2ddbc8c341459de4cb308e5ad64214ce
Igor <igor@devfire.io>
2022-12-03 10:38:02 -0500

    working day 2 part 2

--- day-2/target/debug/deps/day_2-64407bcb2d006a45.d
+++
@@ -1,5 +0,0 @@
-/home/ig/Documents/rust/adventofcode2022/day-2/target/debug/deps/day_2-64407bcb2d006a45.rmeta: src/main.rs
-
-/home/ig/Documents/rust/adventofcode2022/day-2/target/debug/deps/day_2-64407bcb2d006a45.d: src/main.rs
-
-src/main.rs:
--- day-2/target/debug/deps/day_2-f70439130b63b927.d
+++
@@ -1,5 +0,0 @@
-/home/ig/Documents/rust/adventofcode2022/day-2/target/debug/deps/day_2-f70439130b63b927: src/main.rs
-
-/home/ig/Documents/rust/adventofcode2022/day-2/target/debug/deps/day_2-f70439130b63b927.d: src/main.rs
-
-src/main.rs:
