8a94cae868c0d0536c362f0dac8ccdc733cf3c09
Igor <igor@devfire.io>
2022-12-04 23:13:03 -0500

    part 2 works

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -3,6 +3,7 @@ use std::fs::File;
 // use std::hash::Hash;
 // use std::hash;
 use std::io::{self, prelude::*, BufReader};
+use std::vec;
 fn main() -> io::Result<()> {
 
     let file = File::open("input.txt")?;
@@ -11,25 +12,48 @@ fn main() -> io::Result<()> {
     // sum of the priorities
     let mut total_value = 0;
 
+    let mut line_count = 0;
+
+    let mut vector_of_sets: Vec<HashSet<char>> = vec![];
+
     for line in reader.lines().flatten() {
-        // calculate the middle of the string
-        let middle = line.len() / 2;
-
-        // split the string down the middle
-        let (first, second) = line.split_at(middle);
+        // assign the line to a hash set
+        vector_of_sets.push(add_to_hash_set(line));
         
-        // assign the split strings to two hash sets
-        let hash_set_first = add_to_hash_set(first.to_string());
-        let hash_set_second = add_to_hash_set(second.to_string());
-
-        for item in hash_set_first.intersection(&hash_set_second) {
-            total_value += get_item_value(item);
-            println!("Common item: {} value: {}", item, total_value);
+        line_count += 1;
+        
+        if line_count > 2 {
+            for c in intersection(vector_of_sets) {
+                total_value += get_item_value(&c);
+            }
+            // let c: char = intersection(vector_of_sets).get(value)
+            // println!("Intersection of all: {:?}", intersection(vector_of_sets).get(&0));
+            vector_of_sets = vec![];
+            line_count = 0;
         }
     }
 
+    println!("Total is {}", total_value);
+
     
     Ok(())
+}
+
+
+fn intersection(mut sets: Vec<HashSet<char>>) -> HashSet<char> {
+    if sets.is_empty() {
+        return HashSet::new();
+    }
+    
+    if sets.len() == 1 {
+        return sets.pop().unwrap();
+    }
+    
+    let mut result = sets.pop().unwrap();
+    result.retain(|item| {
+        sets.iter().all(|set| set.contains(item))
+    });
+    result
 }
 
 fn add_to_hash_set (line: String) -> HashSet<char> {
